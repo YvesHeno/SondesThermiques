e018bd32e37ec39d37a425b80baec487333008b3
YvesHeno <louis.jouvin@laposte.net>
2025-10-15 12:24:29 +0200

    prog fonctionnel

--- Scripts/Localisation_sondes.R
+++ Scripts/Localisation_sondes.R
@@ -6,11 +6,13 @@ Localisation_sondes <- function()
   library(sf)
   library(mapview)
   
-  siteshydrometrie <- get_hydrometrie_stations(code_departement=c("22","29","35","56")) %>% 
-    st_as_sf( coords=c("longitude_station","latitude_station"),crs=4326)
   codes_sites_sondes <- c("J750061001","J863241001","J340302001","J510221001","J181301001")
   #il faudra passer par un fichier csv ce serait mieux
-  sondesth <- filter(siteshydrometrie,code_station %in% (codes_sites_sondes))
-  return(list(siteshydrometrie,sondesth))
+  
+  siteshydrometrie <- get_hydrometrie_stations(code_departement=c("22","29","35","56")) %>% 
+    st_as_sf( coords=c("longitude_station","latitude_station"),crs=4326) %>% 
+    mutate(sonde_thermique=if_else(.$code_station %in% codes_sites_sondes,'OUI','NON'),.after=libelle_station)
+
+  return(siteshydrometrie)
 
   }
